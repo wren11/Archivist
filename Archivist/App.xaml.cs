0e9835148ac8d0d0d62ccf4278245a48473328a4
Erik Rogers <erik.rogers@live.com>
2016-04-29 00:35:00 -0400

    Add main view model and bindings

--- Archivist/App.xaml.cs
+++ Archivist/App.xaml.cs
@@ -1,16 +1,24 @@
 ﻿using System.Windows;
 
+using Archivist.ViewModels;
 using Archivist.Views;
 
 namespace Archivist
 {
     public partial class App : Application
     {
+        public static readonly string ApplicationName = "Archivist";
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
+            var viewModel = new MainViewModel { DisplayName = ApplicationName };
             var window = new MainWindow();
+
+            viewModel.CloseRequested += delegate { window.Close(); };
+
+            window.DataContext = viewModel;
             window.ShowDialog();
         }
     }
---
+++ Archivist/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Input;
+
+using Archivist.Commands;
+
+namespace Archivist.ViewModels
+{
+    public sealed class MainViewModel : WorkspaceViewModel
+    {
+        public MainViewModel() { }
+
+        // TODO: Actually do things here
+    }
+}
