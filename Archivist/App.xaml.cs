e33c705caade33f6e5697e2d544634bf5612a06f
Erik Rogers <erik.rogers@live.com>
2016-04-28 22:39:09 -0400

    Add initial WPF project

---
+++ Archivist/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+using Archivist.Views;
+
+namespace Archivist
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var window = new MainWindow();
+            window.ShowDialog();
+        }
+    }
+}
