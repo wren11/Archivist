06341da4fc236423a0b22d3c53c88246c8a550c5
Erik Rogers <erik.rogers@live.com>
2016-04-29 00:29:31 -0400

    Add base class for closeable view models

---
+++ Archivist/ViewModels/WorkspaceViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Input;
+
+using Archivist.Commands;
+
+namespace Archivist.ViewModels
+{
+    public abstract class WorkspaceViewModel : ViewModelBase
+    {
+        public event EventHandler CloseRequested;
+
+        public ICommand CloseCommand => new DelegateCommand(OnCloseRequested);
+
+        protected WorkspaceViewModel() { }
+
+        protected virtual void OnCloseRequested()
+        {
+            CloseRequested?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
