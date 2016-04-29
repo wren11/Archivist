30784f9cc719be44e073722900d0967383b552fc
Erik Rogers <erik.rogers@live.com>
2016-04-28 23:41:09 -0400

    Add observable class for watching property changes

---
+++ Archivist/Common/ObservableObject.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Archivist.Common
+{
+    public abstract class ObservableObject : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        protected ObservableObject() { }
+        
+        protected virtual bool SetProperty<T>(ref T field, T value,
+            [CallerMemberName] string propertyName = "",
+            Action onChanged = null,
+            Action<T> onChanging = null)
+        {
+            // If the current value and new value are the same, do nothing
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return false;
+
+            // Notify that the property is about to change
+            onChanging?.Invoke(value);
+            OnPropertyChanging(propertyName);
+
+            field = value;
+
+            // Notify that the property has changed
+            onChanged?.Invoke();
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+
+        #region INotifyPropertyChanged Methods
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (propertyName == null)
+                throw new ArgumentNullException(nameof(propertyName));
+
+            OnPropertyChanged(propertyName);
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region INotifyPropertyChanging Methods
+        public void NotifyPropertyChanging([CallerMemberName] string propertyName = "")
+        {
+            if (propertyName == null)
+                throw new ArgumentNullException(nameof(propertyName));
+        }
+
+        protected virtual void OnPropertyChanging([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
